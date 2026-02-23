e07c7b965d60be3f0c1c41cf15e1132871196f9b
St John, Austin <pokeboy7to8@gmail.com>
2026-02-22 18:38:31 -0800

    Finished

--- SoftwareStore.cpp
+++ SoftwareStore.cpp
@@ -3,14 +3,38 @@
 using namespace std;
 int main()
 {
-    double mph = 6.5;
-    // PACE calculation
-    /* write your code */
+  double retailPrice = 199.00;
+  int quantity;
+  double discountRate = 0.0;
 
-    //express pace in minutes and seconds
-  /* write your code here */
+  cout << "Enter how much was purchased: ";
+  cin >> quantity;
 
-  //express your output in 1 decimal points
+  if(quantity >= 10){
+    discountRate = 0.20;
+  }
+  else if (quantity >= 20){
+    discountRate = 0.30;
+  }
+  else if (quantity >= 50){
+    discountRate = 0.40;
+  }
+  else if (quantity >= 100){
+    discountRate = 0.50;
+  }
+  else{
+    discountRate = 0.00;
+  }
 
+  double subTotal = quantity * retailPrice;
+  double discount = subTotal * discountRate;
+  double total = subTotal - discount;
 
+  cout<< fixed << setprecision(2);
+  cout << " Your total is: $"<<total<<endl;
+  
+  return 0;
 }
+
+
+
